aac3ede9142368d2aab3f1676baee62e65f212ed
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-09-05 21:29:23 -0300

    Check if path exists

--- ExportMediaInfoAsCsv.ps1
+++ ExportMediaInfoAsCsv.ps1
@@ -1,10 +1,21 @@
 Param
-    (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        [string[]]$Path,
- 
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)] 
-        [string]$Destination
-    )
+(
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string[]]$Path,
 
-    Get-ChildItem -Path $Path -Filter *.mp3 -Recurse -File | ForEach-Object { Get-MediaInfo $_.FullName | Select-Object Performer,Album,Track } | Export-Csv -Path $Destination
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)] 
+    [string]$FileName
+)
+
+if ((Test-Path -Path $Path) -eq $false) {
+    Write-Host "$Path not found" -ForegroundColor Red
+    Exit 1
+} 
+
+try {
+    Get-ChildItem -Path $Path -Filter *.mp3 -Recurse -File | ForEach-Object { Get-MediaInfo $_.FullName | Select-Object Performer,Album,Track } | Export-Csv -Path $FileName
+}
+catch {
+    Write-Host "$($PSItem.ToString())" -ForegroundColor Red
+}
+
